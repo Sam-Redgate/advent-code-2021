2d0cde4ddee64c60cdd8aec0c9f930ee3ccd8b91
Sam-Redgate <Samuel.Comer@red-gate.com>
2021-12-01 12:10:22 +0000

    Day 1 Star 1

---
+++ AdventOfCode/DayOne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DayOne
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine(CountDepthIncreases(File.ReadLines("./Resources/input.txt")));
+        }
+
+        private static int CountDepthIncreases(IEnumerable<string> depthMeasurements) => CountDepthIncreases(depthMeasurements.Select(int.Parse));
+
+        private static int CountDepthIncreases(IEnumerable<int> depthMeasurements)
+        {
+            var currentMeasurements = depthMeasurements as int[] ?? depthMeasurements.ToArray();
+
+            if (currentMeasurements.Length < 2)
+            {
+                return 0;
+            }
+
+            var count = 0;
+
+            int? previousMeasurement = int.MaxValue;
+            foreach (var currentMeasurement in currentMeasurements)
+            {
+                if (currentMeasurement > previousMeasurement)
+                {
+                    count++;
+                }
+
+                previousMeasurement = currentMeasurement;
+            }
+
+            return count;
+        }
+    }
+}
