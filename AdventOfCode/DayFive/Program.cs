bd9c93f6e141d22028e7ada9eb0d127bf9db02b0
Samuel Comer <samuel.comer@red-gate.com>
2021-12-05 09:49:52 +0000

    Day 5 Star 1

---
+++ AdventOfCode/DayFive/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace DayFive
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var map = new GeothermalVentMap(File.ReadLines("./Resources/input.txt"));
+            Console.Out.WriteLine($"Number of overlapping points: {map.CountOverlappingPoints()}");
+        }
+    }
+}
