b69aae24b3193914e95290da01bbc61547f3ad12
Samuel Comer <samuel.comer@red-gate.com>
2021-12-08 19:18:56 +0000

    Day 8 Star 1

---
+++ AdventOfCode/DayEight/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DayEight
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(ParseValues(File.ReadLines("./Resources/input.txt")).Aggregate(0, (sum, values) => sum + CountUniqueDigits(values.Split())));
+        }
+
+        private static IEnumerable<string> ParseValues(IEnumerable<string> inputs)
+        {
+            return inputs.Select(i => i.Split('|')[1].Trim());
+        }
+
+        private static int CountUniqueDigits(IEnumerable<string> inputs)
+        {
+            return inputs.Count(i => i.Length is 2 or 4 or 3 or 7);
+        }
+    }
+}
