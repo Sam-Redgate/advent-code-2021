d45d5b7a3ee0adf5fcba2a4932ae622f6317322c
Samuel Comer <samuel.comer@red-gate.com>
2021-12-12 16:36:05 +0000

    Day 12 Star 2

--- AdventOfCode/Code/DayTwelve/DayTwelve.cs
+++ AdventOfCode/Code/DayTwelve/DayTwelve.cs
@@ -1,31 +1,20 @@
 namespace Code;
 
-public class DayTwelve
+public static class DayTwelve
 {
     public static void Run()
     {
         var startValue = "start";
         var endValue = "end";
-        var edges = ParseEdges(File.ReadAllLines("./Resources/DayTwelveTest.txt"));
+        var edges = ParseEdges(File.ReadAllLines("./Resources/DayTwelveInput.txt"));
 
-        var tree = Tree<string>.BuildTree(startValue, edges, GetValidDuplicateFilter(), GetLeafValueCheck(endValue));
+        var tree = PathTree.BuildTree(startValue, edges);
         
-        tree.PrintTree();
         Console.WriteLine(tree.CountValue(endValue));
     }
 
     private static IEnumerable<(string, string)> ParseEdges(IEnumerable<string> input)
     {
         return input.Select(line => (line.Split('-')[0], line.Split('-')[1]));
-    }
-
-    private static Func<string, string, bool> GetValidDuplicateFilter()
-    {
-        return (left, right) => !left.Equals(right.ToLower());
-    }
-
-    private static Func<string, bool> GetLeafValueCheck(string endValue)
-    {
-        return value => value.Equals(endValue);
     }
 }
