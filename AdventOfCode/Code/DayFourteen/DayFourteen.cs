3011f4b183c756a5431e953e6246dc4d5dd4f833
Samuel Comer <samuel.comer@red-gate.com>
2021-12-14 14:33:28 +0000

    Day 14 Star 2

--- AdventOfCode/Code/DayFourteen/DayFourteen.cs
+++ AdventOfCode/Code/DayFourteen/DayFourteen.cs
@@ -1,66 +1,103 @@
-using System.Text;
-
 namespace Code;
 
 public static class DayFourteen
 {
     public static void Run()
     {
-        var input = File.ReadLines("./Resources/DayFourteenTest.txt").ToArray();
+        var input = File.ReadLines("./Resources/DayFourteenInput.txt").ToArray();
 
-        const int steps = 10;
+        const int steps = 40;
         var template = input.First();
-        var rules = input.Where(row => row.Contains("->")).ToDictionary(row => row.Split(' ')[0], row => row.Split(' ')[^1]);
+        var rules = ParseRules(input);
 
-        var polymerElements = CalculatePolymer(template, rules, steps).ToCharArray();
+        var (leastCommonElementCount, mostCommonElementCount) = FindLeastAndMostCommonElement(template, rules, steps);
 
-        var (leastCommonElementCount, mostCommonElementCount) = FindLeastAndMostCommonElement(polymerElements);
-        
         Console.WriteLine($"Result: {mostCommonElementCount - leastCommonElementCount}");
     }
 
-    private static (int, int) FindLeastAndMostCommonElement(char[] polymerElements)
+    private static (ulong, ulong) FindLeastAndMostCommonElement(string template, Dictionary<string, char> rules,
+        int steps)
     {
-        var elementCounts = new Dictionary<char, int>();
-        foreach (var element in polymerElements)
-        {
-            if (!elementCounts.ContainsKey(element)) elementCounts[element] = 1;
-            else elementCounts[element] += 1;
-        }
-
-        var counts = elementCounts.Values.OrderBy(v => v).ToArray();
-        return (counts[0], counts[^1]);
-    }
-
-    private static string CalculatePolymer(string template, Dictionary<string,string> rules, int steps)
-    {
+        var characterCounts = MapCharacters(template);
+        var pairCounts = InitializePairCountFromTemplate(template);
 
         while (steps-- > 0)
         {
-            var builder = new StringBuilder(template[..1]);
+            // keep a record of all the new pairs created so we can apply them "simultaneously"
+            var ruleOutput = new Dictionary<string, ulong>();
             
-            foreach (var pair in IteratePairs(template))
+            foreach (var rule in rules.Keys.Where(rule => pairCounts.ContainsKey(rule) && pairCounts[rule] > 0))
             {
-                if (rules.ContainsKey(pair))
-                {
-                    builder.Append($"{rules[pair]}{pair.ToCharArray()[1]}");
-                    continue;
-                }
+                var (leftPair, rightPair) = CreateRuleOutput(rule, rules[rule]);
 
-                builder.Append($"{pair.ToCharArray()[1]}");
+                SumDictionaryElements(ruleOutput, leftPair, pairCounts, rule);
+
+                SumDictionaryElements(ruleOutput, rightPair, pairCounts, rule);
+
+                SumDictionaryElements(characterCounts, rules[rule], pairCounts, rule);
+
+                pairCounts[rule] = 0;
             }
 
-            template = builder.ToString();
+            pairCounts = AggregatePairs(pairCounts, ruleOutput);
         }
 
-        return template;
+        var orderedCounts = characterCounts.Values.OrderBy(v => v).ToArray();
+        
+        return (orderedCounts[0], orderedCounts[^1]);
+    }
+
+    private static Dictionary<string, ulong> InitializePairCountFromTemplate(string template)
+    {
+        return MapElementsCountToDictionary(IteratePairs(template).ToArray());
+    }
+
+    private static Dictionary<char, ulong> MapCharacters(string input)
+    {
+        return MapElementsCountToDictionary(input.ToCharArray());
+    }
+
+    private static Dictionary<T, ulong> MapElementsCountToDictionary<T>(T[] elements) where T : IEquatable<T>
+    {
+        return elements.ToHashSet().ToDictionary(key => key, key => (ulong) elements.Count(key.Equals));
+    }
+
+    private static Dictionary<string, ulong> AggregatePairs(Dictionary<string, ulong> destination,
+        Dictionary<string, ulong> source)
+    {
+        foreach (var pair in source.Keys)
+        {
+            SumDictionaryElements(destination, pair, source, pair);
+        }
+
+        return destination;
+    }
+
+    private static void SumDictionaryElements<T1, T2>(IDictionary<T1, ulong> destination, T1 destinationKey,
+        IReadOnlyDictionary<T2, ulong> source, T2 sourceKey)
+    {
+        if (destination.ContainsKey(destinationKey)) destination[destinationKey] += source[sourceKey];
+        else destination[destinationKey] = source[sourceKey];
+    }
+
+    private static (string, string) CreateRuleOutput(string pair, char result)
+    {
+        var left = pair[0];
+        var right = pair[^1];
+        return ($"{left}{result}", $"{result}{right}");
     }
 
     private static IEnumerable<string> IteratePairs(string template)
     {
         for (var i = 0; i < template.Length - 1; i++)
         {
-            yield return template[i..(i+2)];
+            yield return template[i..(i + 2)];
         }
+    }
+
+    private static Dictionary<string, char> ParseRules(IEnumerable<string> input)
+    {
+        return input.Where(row => row.Contains("->"))
+            .ToDictionary(row => row.Split(' ')[0], row => row.Split(' ')[^1].ToCharArray()[0]);
     }
 }
