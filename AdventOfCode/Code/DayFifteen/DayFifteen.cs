e250c3397533430cbfb8723dc56d8d536ab907f8
Samuel Comer <samuel.comer@red-gate.com>
2021-12-15 10:19:13 +0000

    Day 15 Star 2

--- AdventOfCode/Code/DayFifteen/DayFifteen.cs
+++ AdventOfCode/Code/DayFifteen/DayFifteen.cs
@@ -1,20 +1,23 @@
 namespace Code;
 
-public class DayFifteen
+public static class DayFifteen
 {
     public static void Run()
     {
-        var map = ParseMap(File.ReadLines("./Resources/DayFifteenInput.txt").ToArray());
-        var shortestCost = GetShortestPathCost(map, new Coordinate(0, 0), new Coordinate(map.GetLength(1) - 1, map.GetLength(0) - 1));
+        var page = ParseMap(File.ReadLines("./Resources/DayFifteenInput.txt").ToArray());
+        var map = RebuildMapFromPages(page, 5, 5);
+
+        var shortestCost =
+            GetShortestPathCost(map, new Coordinate(0, 0), new Coordinate(map[0].Length - 1, map.Length - 1));
         Console.WriteLine(shortestCost);
     }
 
-    // Dijkstra
-    private static int GetShortestPathCost(int[,] map, Coordinate start, Coordinate goal)
+    // Dijkstra, though it really should be A* I think, which should be quicker.
+    private static int GetShortestPathCost(int[][] map, Coordinate start, Coordinate goal)
     {
         var costLookup = BuildCoordinateSetOfCosts(map);
         costLookup[start] = 0;
-        
+
         var remainingCoordinates = costLookup.ToDictionary(pair => pair.Key, _ => int.MaxValue);
 
         while (remainingCoordinates.Count > 0)
@@ -25,9 +28,9 @@ public class DayFifteen
             foreach (var neighbour in GetNeighbours(cheapestNode, remainingCoordinates))
             {
                 var nextNodeCost = costLookup[cheapestNode] + costLookup[neighbour];
-                
+
                 if (nextNodeCost >= remainingCoordinates[neighbour]) continue;
-                
+
                 costLookup[neighbour] = nextNodeCost;
                 remainingCoordinates[neighbour] = nextNodeCost;
             }
@@ -36,61 +39,96 @@ public class DayFifteen
         return costLookup[goal];
     }
 
-    private static IEnumerable<Coordinate> GetNeighbours(Coordinate source, Dictionary<Coordinate,int> nodes)
+    private static IEnumerable<Coordinate> GetNeighbours(Coordinate source, Dictionary<Coordinate, int> nodes)
     {
         var (x, y) = source;
-        
+
         if (nodes.ContainsKey(new Coordinate(x, y - 1)))
             yield return new Coordinate(x, y - 1);
-        
+
         if (nodes.ContainsKey(new Coordinate(x + 1, y)))
             yield return new Coordinate(x + 1, y);
-        
+
         if (nodes.ContainsKey(new Coordinate(x, y + 1)))
             yield return new Coordinate(x, y + 1);
-        
+
         if (nodes.ContainsKey(new Coordinate(x - 1, y)))
             yield return new Coordinate(x - 1, y);
     }
 
-    private static Dictionary<Coordinate, int> BuildCoordinateSetOfCosts(int[,] grid)
+    private static Dictionary<Coordinate, int> BuildCoordinateSetOfCosts(int[][] grid)
     {
         var coordinateCosts = new Dictionary<Coordinate, int>();
 
         foreach (var coordinate in GridIterator(grid))
         {
-            coordinateCosts[coordinate] = grid[coordinate.Y, coordinate.X];
+            coordinateCosts[coordinate] = grid[coordinate.Y][coordinate.X];
         }
 
         return coordinateCosts;
     }
 
-    private static int[,] ParseMap(IReadOnlyList<string> rows)
+    private static int[][] RebuildMapFromPages(int[][] page, int pageHeight, int pageWidth)
+    {
+        var destination = new int[pageHeight * page.Length][];
+
+        while (pageHeight-- > 0)
+        {
+            var yOffset = pageHeight * page.Length;
+            for (var y = 0; y < page.Length; y++)
+            {
+                destination[y + yOffset] = CopyRow(page[y], pageHeight, pageWidth);
+            }
+        }
+
+        return destination;
+    }
+
+    private static int[] CopyRow(IReadOnlyList<int> row, int offset, int width)
+    {
+        var destination = new int[row.Count * width];
+        while (width-- > 0)
+        {
+            var xOffset = width * row.Count;
+            for (var x = 0; x < row.Count; x++)
+            {
+                destination[x + xOffset] = AddRiskOffset(row[x], offset + width);
+            }
+        }
+
+        return destination;
+    }
+
+    private static int AddRiskOffset(int value, int valueOffset)
+    {
+        var remainder = (value + valueOffset) % 9;
+        return value + valueOffset > 9 ? remainder : value + valueOffset;
+    }
+
+    private static int[][] ParseMap(IReadOnlyList<string> rows)
     {
         var maxWidth = rows[0].Length;
         var maxHeight = rows.Count;
-        var grid = new int[maxHeight, maxWidth];
+        var grid = new int[maxHeight][];
 
         for (var y = 0; y < maxHeight; y++)
         {
+            grid[y] = new int[maxWidth];
             var row = rows[y].ToCharArray();
             for (var x = 0; x < maxWidth; x++)
             {
-                grid[y, x] = Convert.ToInt32(row[x].ToString());
+                grid[y][x] = Convert.ToInt32(row[x].ToString());
             }
         }
 
         return grid;
     }
 
-    private static IEnumerable<Coordinate> GridIterator<T>(T[,] page)
+    private static IEnumerable<Coordinate> GridIterator<T>(IReadOnlyList<T[]> page)
     {
-        var maxHeight = page.GetLength(0);
-        var maxWidth = page.GetLength(1);
-
-        for (var y = 0; y < maxHeight; y++)
+        for (var y = 0; y < page.Count; y++)
         {
-            for (var x = 0; x < maxWidth; x++)
+            for (var x = 0; x < page[y].Length; x++)
             {
                 yield return new Coordinate(x, y);
             }
